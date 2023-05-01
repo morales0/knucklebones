0ac4b14e163f2774dca64804161f4363deea0b26
Francisco Morales <chico.fmorales@gmail.com>
2023-04-30 19:46:43 -0700

    connected server and client

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import "./App.css";
+
+const socket = io("localhost:3000");
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [messages, setMessages] = useState<string[]>([])
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    // When server connects with us, log and send message
+    socket.on("connect", () => {
+      console.log("conncected", socket);
+      socket.emit("message", `Hi, I'm ${socket.id}`)
+    });
+
+    // When server disconnects with us, log
+    socket.on("disconnect", () => {
+      console.log("disconnected");
+    });
+
+    // When server sends us a "message" event, add to screen
+    socket.on("message", (msg: string) => {
+      setMessages((prev) => [...prev, msg])
+    })
+
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("message");
+    };
+  }, []);
+
+  return <div>
+    {messages.map((msg, i) => <div key={`message-${i}`}>{msg}</div>)}
+  </div>;
 }
 
-export default App
+export default App;
