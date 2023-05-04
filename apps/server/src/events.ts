fd31d209cf422e48ba5768bd0374bcfc96c085c4
Francisco Morales <chico.fmorales@gmail.com>
2023-05-03 22:34:36 -0700

    created some skeleton code for server

---
+++ apps/server/src/events.ts
@@ -0,0 +1,11 @@
+export const JOIN_ROOM = "join_room";
+export const MAKE_ROOM = "make_room";
+export const LEAVE_ROOM = "leave_room";
+
+export const START_GAME = "start_game";
+
+export const ROLL_DICE = "roll_dice";
+export const PLAY_DICE = "play_dice";
+export const WIN = "win";
+
+export const UPDATE_USERNAME = "update_username";
--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -2,6 +2,15 @@ import { type Request, type Response } from "express";
 import app from "./app";
 import http from "http";
 import { Server } from "socket.io";
+import {
+  START_GAME,
+  ROLL_DICE,
+  PLAY_DICE,
+  UPDATE_USERNAME,
+  JOIN_ROOM,
+  MAKE_ROOM,
+  LEAVE_ROOM,
+} from "./events";
 
 const server = http.createServer(app);
 const io = new Server(server);
@@ -19,6 +28,56 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log(`${socket.id} disconnected`);
     io.emit("message", `${socket.id} left`);
+  });
+
+  // Player events
+  socket.on(UPDATE_USERNAME, (username: string) => {
+    // Update player state/object
+    // Update room state/object
+    // Emit new state
+  });
+
+  socket.on(JOIN_ROOM, (roomId: string) => {
+    // Search for room
+    // Make sure user isn't in room
+    // Add user to room
+    // Notify other members
+  });
+
+  socket.on(MAKE_ROOM, () => {
+    // Create new ID
+    // Create room
+    // Add user to room
+    // Send user room ID
+  });
+
+  socket.on(LEAVE_ROOM, () => {
+    // Find what room user is in
+    // Remove user from room
+    // Emit to other members
+    // If room is empty, delete room
+  });
+
+  // Gameplay events
+
+  socket.on(START_GAME, () => {
+    // Get room ID
+    // Init game state
+    // Send start game to room
+  });
+
+  socket.on(ROLL_DICE, () => {
+    // Get room ID
+    // Roll random number
+    // Send number to all members in room
+  });
+
+  socket.on(PLAY_DICE, (cell: number[]) => {
+    // Get room ID
+    // Update game state grid
+    // Check if this is a winning move
+    // If end game, emit to room
+    // Send game state grid to room
   });
 
   // When client sends "message" event, emit to all other connections
