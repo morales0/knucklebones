ee8a1fde913424cf65cca9a12a43182456cbaeff
Francisco Morales <chico.fmorales@gmail.com>
2023-05-12 20:20:17 -0700

    styled dice

--- apps/frontend/src/components/dice.tsx
+++ apps/frontend/src/components/dice.tsx
@@ -1,9 +1,17 @@
+import "../styles/dice.css";
+
 type Props = {
   value: number;
 };
 
 const Dice = ({ value }: Props) => {
-  return <div className="cell dice">{value}</div>;
+  return (
+    <div className={`cell dice dice_${value}`}>
+      {[...new Array(value)].map((_, i) => (
+        <div className="dot" />
+      ))}
+    </div>
+  );
 };
 
 export default Dice;
